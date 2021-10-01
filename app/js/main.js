c5d237c90ed6211e65a437fac30d6e6f554a1ce5
kukuvz <antonslayer@gmail.com>
2021-10-01 16:25:24 +0300

    added 2 pages product-list with filters

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,38 @@
 $(function() {
+
+  //star ratings in recent products
+  $(".recent-products__star").rateYo({
+    starWidth: "11px",
+    spacing: "9px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true,
+  });
+
+  //star ratings in products
+  $(".star").rateYo({
+    starWidth: "16px",
+    spacing: "14px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true,
+  });
+
+  //ionRangeSlider in filter price on Shop page
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    step : 0.01,
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },  
+  });
+
   //main slider
   $('.top-slider__inner').slick({
     dots: true,
